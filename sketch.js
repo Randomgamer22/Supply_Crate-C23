731e8f9b4fddfa189320cfd014154e59378115e2
Randomgamer22 <63390136+Randomgamer22@users.noreply.github.com>
2020-09-13 18:43:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var supplyPackage, packageImage;
+var helicopter;
+var ground;
+var redBox1, redBox2, redBox3;
+
+function preload() {
+    packageImage = loadImage("./package.png");
+    helicopter = loadImage("./helicopter.png");
+}
+
+function setup() {
+    createCanvas(400, 400);
+
+    engine = Engine.create();
+    world = engine.world;
+
+    options = {
+        isStatic: true
+    }
+
+    supplyPackage = Bodies.rectangle(200, 100, 30, 30, options);
+    ground = Bodies.rectangle(200, 400, 400, 50, {isStatic: true});
+    redBox1 = Bodies.rectangle(200, 365, 100, 20, {isStatic: true});
+    redBox2 = Bodies.rectangle(260, 325, 20, 100, {isStatic: true});
+    redBox3 = Bodies.rectangle(140, 325, 20, 100, {isStatic: true});
+
+    World.add(world, supplyPackage);
+    World.add(world, ground);
+    World.add(world, redBox1);
+    World.add(world, redBox2);
+    World.add(world, redBox3);
+}
+
+function draw() {
+    background(0);
+
+    imageMode(CENTER);
+    image(packageImage, supplyPackage.position.x, supplyPackage.position.y, 30, 30);
+
+    image(helicopter, 200, 100, 110, 55);
+
+    if(keyDown("down")){
+        Matter.Body.setStatic(supplyPackage, false);
+    }
+
+    push();
+    fill("brown");
+    rectMode(CENTER);
+    rect(ground.position.x, ground.position.y, 400, 50);
+    pop();
+
+    push();
+    fill("red");
+    rectMode(CENTER);
+    rect(redBox1.position.x, redBox1.position.y, 100, 20);
+    rect(redBox2.position.x, redBox2.position.y, 20, 100);
+    rect(redBox3.position.x, redBox3.position.y, 20, 100);
+    pop();
+
+    Engine.update(engine);
+}
